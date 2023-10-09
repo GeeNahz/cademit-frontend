66f0e9fbfbc0f8a6de27eb8843ffea714c3bff1c
Godswill Nasara <godswillnlm@gmail.com>
2023-10-09 17:30:22 +0100

    Feature: create route template for basic CRUD ops on profile model

---
+++ app/api/pg-cohort/[id]/route.ts
@@ -0,0 +1,7 @@
+import { Types } from "mongoose";
+
+export const GET = async (res: Request, { params }: { params: Types.ObjectId}) => {}
+
+export const PATCH = async (res: Request, { params }: { params: Types.ObjectId}) => {}
+
+export const DELETE = async (res: Request, { params }: { params: Types.ObjectId}) => {}
