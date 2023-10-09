d8c7d96dccb8cb46b0469d3b983218711619a086
Godswill Nasara <godswillnlm@gmail.com>
2023-10-09 19:18:55 +0100

    Feature: wrap dashboard layout with next auth provider

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -2,6 +2,7 @@ import '@/app/globals.css'
 
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import Provider from '../components/Provider'
 
 export default function HomeLayout({
   children,
@@ -9,10 +10,12 @@ export default function HomeLayout({
   children: React.ReactNode
 }) {
   return (
+    <Provider>
       <section className="font-inter bg-[#F8F8F8] text-neutral">
         <Navbar />
         {children}
         <Footer />
       </section>
+    </Provider>
     )
 }
