d049957c68598c80776560231c3890aabda14b0f
Godswill Nasara <godswillnlm@gmail.com>
2023-10-09 19:21:31 +0100

    Feature: create layout for authentication pages

---
+++ app/signup/layout.tsx
@@ -0,0 +1,5 @@
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <section className="bg-[#F8F8F8] text-neutral">{ children }</section>
+    );
+}
