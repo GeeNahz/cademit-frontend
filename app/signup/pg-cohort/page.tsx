c5b1d1f4a33d8451e1cea1243d417558671cd385
Godswill Nasara <godswillnlm@gmail.com>
2023-10-09 20:35:33 +0100

    Style: edit form title

--- app/signup/pg-cohort/page.tsx
+++ app/signup/pg-cohort/page.tsx
@@ -3,7 +3,7 @@ import RegisterForm from "@/app/components/RegisterForm";
 function PgSignup() {
     return (
         <div>
-            <RegisterForm submitUrl="/api/pg-cohort/signup" formTitle="PG Cohort Signup" formFooter={<span>Sign up for the Data science PG Cohort program. <a href="#" className='link link-info'>Learn more.</a></span>} />
+            <RegisterForm submitUrl="/api/pg-cohort/signup" formTitle="PG Cohort" formFooter={<span>Sign up for the Data science PG Cohort program. <a href="#" className='link link-info'>Learn more.</a></span>} />
         </div>
     )
 }
