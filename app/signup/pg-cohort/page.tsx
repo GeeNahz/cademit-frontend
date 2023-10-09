fcb37192405b3898aab751dc59fcfd28ce0e1de2
Godswill Nasara <godswillnlm@gmail.com>
2023-10-09 17:11:42 +0100

    Feature: create signup page for pg-cohort program

---
+++ app/signup/pg-cohort/page.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from "@/components/RegisterForm";
+
+function PgSignup() {
+    return (
+        <div>
+            <RegisterForm submitUrl="/api/pg-cohort/signup" formTitle="PG Cohort Signup" formFooter={<span>Sign up for the Data science PG Cohort program. <a href="#" className='link link-info'>Learn more.</a></span>} />
+        </div>
+    )
+}
+
+export default PgSignup;
