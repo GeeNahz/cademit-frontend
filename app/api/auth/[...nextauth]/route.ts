55b3dd9a96a63b852b2eeda000fa0e209594d106
Godswill Nasara <godswillnlm@gmail.com>
2023-10-09 17:20:03 +0100

    Feature: create authentication route

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,69 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+import User from "@/models/user"
+
+import { connectToDB } from "@/utils/database"
+import { Profile, Session, AuthOptions } from "next-auth/core/types";
+
+type ProfilePlus = Profile & {
+  username: string;
+  picture: string;
+}
+
+type SessionPlus = Session & {
+  user?: {
+    id?: string | null;
+    name?: string | null
+    email?: string | null
+    image?: string | null
+  }
+}
+
+export const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: "",
+      clientSecret: "",
+    })
+  ],
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session?.user?.email,
+      });
+  
+      (session as SessionPlus).user!.id = sessionUser._id.toString();
+  
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+  
+        // Check if user already exists
+        const userExists = await User.findOne({
+          email: profile?.email,
+        });
+  
+        // if not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile?.email,
+            username: (profile as ProfilePlus)?.username.replace(" ", "").toLowerCase(),
+            image: (profile as ProfilePlus)?.picture,
+          });
+        }
+  
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
