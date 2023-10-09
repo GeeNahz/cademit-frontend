81c9e0f8c5aabbbd9af7e5cc00f892f26c281dad
Godswill Nasara <godswillnlm@gmail.com>
2023-10-09 17:12:23 +0100

    Feature: create base signup page

---
+++ app/signup/components/SignupCard.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface Props {
+    imageUrl: string;
+    title: string;
+    link: string;
+}
+
+const SignupCard = ({ imageUrl, title, link } : Props) => {
+    return (
+        <Link href={link} className="card w-full sm:w-96 shadow-xl bg-white hover:bg-primary-content transition-colors duration-200">
+            <div className="min-h-[340px]">
+                <figure className="px-10 pt-10">
+                    <Image
+                        src={imageUrl}
+                        alt={title}
+                        width={305}
+                        height={304}
+                        fill={false}
+                        className="rounded-xl w-auto h-auto"
+                    />
+                </figure>
+            </div>
+            <div className="card-body items-center text-center">
+                <p>Sign up as</p>
+                <h2 className="card-title text-primary">{ title }</h2>
+                {/* <div className="card-actions">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div> */}
+            </div>
+        </Link>
+    )
+}
+
+export default SignupCard;
---
+++ app/signup/page.tsx
@@ -0,0 +1,54 @@
+import SignupCard from "./components/SignupCard";
+
+interface Option {
+    id: number;
+    title: string;
+    imageUrl: string;
+    link: string;
+}
+
+const Signup = () => {
+    const signupOptions: Option[] = [
+        {
+            id: 1,
+            title: "School/Parent",
+            imageUrl: "/images/parent-illustration.avif",
+            link: "/signup/school-parent",
+        },
+        {
+            id: 2,
+            title: "Educator",
+            imageUrl: "/images/tutor-illustration.avif",
+            link: "/signup/educator",
+        },
+        {
+            id: 3,
+            title: "Individual",
+            imageUrl: "/images/individual-illustration.avif",
+            link: "/signup/individual",
+        },
+    ];
+
+    return (
+        <div className="h-full w-full py-40 px-5 md:px-0">
+
+            <div className="title mb-20">
+                <h1 className="text-4xl font-bold text-center">Who do you want to sign up as?</h1>
+            </div>
+            
+            <div className="flex w-fit mx-auto flex-col md:flex-row flex-wrap justify-center gap-5">
+                {
+                    signupOptions.map((option) => (
+                        <SignupCard
+                            title={option.title}
+                            imageUrl={option.imageUrl}
+                            link={option.link}
+                        />
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Signup;
