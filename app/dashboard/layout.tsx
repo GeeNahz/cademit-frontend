ba4df1b48923e9a37b4ca9b781fee3daa337c444
Godswill Nasara <godswillnlm@gmail.com>
2023-10-09 17:17:06 +0100

    Feature: create layout for dashboard

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,24 @@
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+
+
+export default function DashboardLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <section className='bg-[#F8F8F8] text-neutral relative max-h-screen h-full'>
+            <Navbar />
+
+            <div className="main-wrapper flex w-full">
+                <Sidebar />      
+                <main className='flex-1 max-w-[calc(100%-300px)] flex w-full min-h-[calc(100vh-3.75rem)] h-full overflow-hidden'>
+                    <div className="main-container max-h-[calc(100vh-3.75rem)] overflow-y-scroll p-4">
+                        {children}
+                    </div>
+                </main>
+            </div>
+        </section>
+    )
+}
