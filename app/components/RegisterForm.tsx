60542e2b9885406d1f118706043cb29f4b1e6489
Godswill Nasara <godswillnlm@gmail.com>
2023-10-09 21:25:09 +0100

    Feature: add modules for pay per module

--- app/components/RegisterForm.tsx
+++ app/components/RegisterForm.tsx
@@ -1,6 +1,14 @@
 "use client";
 
-import { FormEvent, useState } from "react";
+import { FormEvent, useState, useEffect } from "react";
+
+
+const PAYMENT_APPROACH = {
+    FULL_PAYMENT: "Full payment",
+    PER_MODULE: "Pay per module",
+};
+
+type PaymentApproach = "Full payment" | "Pay per module" | "";
 
 type FormDataType = {
     first_name: string;
@@ -13,7 +21,9 @@ type FormDataType = {
     computer_access: boolean;
     internet_access: boolean;
     use_workspace: boolean;
-    payment_approach: string;
+    payment_approach: PaymentApproach;
+    module: string;
+    paid: boolean;
 }
 
 type PropsType = {
@@ -31,11 +41,13 @@ function RegisterForm({ submitUrl, formTitle, formDesc, formFooter }: PropsType)
         phone: "",
         course: "",
         employment_status: "",
-        payment_approach: "",
+        payment_approach: "" as PaymentApproach,
         experience_level: 0,
         computer_access: false,
         internet_access: false,
         use_workspace: false,
+        module: "Full payment",
+        paid: false,
     });
 
     const employmentStatus = [
@@ -48,7 +60,7 @@ function RegisterForm({ submitUrl, formTitle, formDesc, formFooter }: PropsType)
         { id: 1, title: "Data Science", },
     ];
 
-    const experienceLevel = [
+    const experienceLevels = [
         {
             id: 1,
             value: 0,
@@ -71,10 +83,40 @@ function RegisterForm({ submitUrl, formTitle, formDesc, formFooter }: PropsType)
         },
     ];
 
-    const paymentMethod = [
-        { id: 1, method: "Full payment", },
-        { id: 2, method: "Pay per module", }
+    const paymentMethods = [
+        { id: 1, method: PAYMENT_APPROACH.FULL_PAYMENT, },
+        { id: 2, method: PAYMENT_APPROACH.PER_MODULE, }
     ];
+
+    const modulePrice = 15550;
+
+    const courseModules = [
+        { id: 1, name: "Beginner", price: modulePrice },
+        { id: 2, name: "Intermediate", price: modulePrice },
+        { id: 3, name: "Advance", price: modulePrice },
+    ];
+
+    const [price, setPrice] = useState(0);
+    const [showModules, setShowModules] = useState(false);
+    
+    useEffect(() => {
+        const handLESetPrice = () => {
+            if (formData.payment_approach === PAYMENT_APPROACH.FULL_PAYMENT) {
+                setPrice(modulePrice * 3);
+                setShowModules(false);
+                formData.module = PAYMENT_APPROACH.FULL_PAYMENT;
+            } else if (formData.payment_approach === PAYMENT_APPROACH.PER_MODULE) {
+                setPrice(modulePrice);
+                setShowModules(true);
+            } else {
+                setPrice(0);
+                setShowModules(false);
+                formData.module = PAYMENT_APPROACH.FULL_PAYMENT;
+            }
+        };
+
+        handLESetPrice();
+    }, [formData.payment_approach]);
 
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
@@ -84,7 +126,7 @@ function RegisterForm({ submitUrl, formTitle, formDesc, formFooter }: PropsType)
                 method: "POST",
                 body: JSON.stringify(formData)
             });
-            
+
             if (response.ok) console.log("RES DATA: ", await response.json());
         } catch (error) {
             console.log(error);
@@ -95,9 +137,9 @@ function RegisterForm({ submitUrl, formTitle, formDesc, formFooter }: PropsType)
         <div className="h-full w-full py-20 px-5 md:px-0">
 
             <div className="title mb-5 text-center">
-                <h1 className="text-4xl font-bold mb-5">{ formTitle } Signup</h1>
+                <h1 className="text-4xl font-bold mb-5">{formTitle} Signup</h1>
                 <p className='text-sm text-slate-500 py-3 block text-center'>
-                    { formDesc && formDesc }
+                    {formDesc && formDesc}
                 </p>
             </div>
 
@@ -157,7 +199,7 @@ function RegisterForm({ submitUrl, formTitle, formDesc, formFooter }: PropsType)
                         <span className="text-slate-400 text-xs">On a scale of 0 - 3, what is your experience level on the course you've chosen?</span>
                         <div className="mt-2">
                             {
-                                experienceLevel.map((exp) => (
+                                experienceLevels.map((exp) => (
                                     <div key={exp.id}>
                                         <label className="inline-flex items-center">
                                             <input className="form-radio focus:ring-0" type="radio" name="experience-level" value={exp.value} required onChange={(e) => setFormData((prev) => ({ ...prev, experience_level: parseInt(e.target.value) }))} />
@@ -169,22 +211,49 @@ function RegisterForm({ submitUrl, formTitle, formDesc, formFooter }: PropsType)
                         </div>
                     </fieldset>
 
-                    <label className="block w-full">
-                        <span className='text-slate-600 text-sm'>Method of payment <span className="text-red-400">*</span></span>
-                        <select name="employment-status" id="employment-status" className="form-select w-full block mt-1 rounded border-zinc-200 focus:border focus:border-primary-focus focus:bg-transparent focus:ring-0" value={formData.payment_approach} onChange={(e) => setFormData((prev) => ({ ...prev, payment_approach: e.target.value }))} required>
-                            <option>-- Select method of payment --</option>
-                            {
-                                paymentMethod.map((method) => (
-                                    <option key={method.id} value={method.method}>{method.method}</option>
-                                ))
-                            }
-                        </select>
-                    </label>
+                    <div>
+                        <label className="block w-full">
+                            <span className='text-slate-600 text-sm'>Method of payment <span className="text-red-400">*</span></span>
+                            <p className="text-xs text-slate-400 mt-1">Each module costs the same.</p>
+                            <select name="employment-status" id="employment-status" className="form-select w-full block mt-1 rounded border-zinc-200 focus:border focus:border-primary-focus focus:bg-transparent focus:ring-0" value={formData.payment_approach} onChange={(e) => setFormData((prev) => ({ ...prev, payment_approach: e.target.value as PaymentApproach }))} required>
+                                <option>-- Select method of payment --</option>
+                                {
+                                    paymentMethods.map((method) => (
+                                        <option key={method.id} value={method.method}>{method.method}</option>
+                                    ))
+                                }
+                            </select>
+                        </label>
+                        
+                        {
+                            showModules && (<div className="modules mt-3">
+                            <fieldset className="block w-full">
+                                <legend className="text-slate-600 text-xs">Select a module</legend>
+                                <div className="mt-2">
+                                    {
+                                        courseModules.map((module) => (
+                                            <div key={module.id}>
+                                                <label className="inline-flex items-center mt-2">
+                                                    <input className="form-radio focus:ring-0" type="radio" name="experience-level" value={module.name} required onChange={(e) => setFormData((prev) => ({ ...prev, module: e.target.value }))} />
+                                                    <p className=" text-sm ml-2">{module.name}</p>
+                                                </label>
+                                            </div>
+                                        ))
+                                    }
+                                </div>
+                            </fieldset>
+                            </div>)
+                        }
+
+                        <p className="text-stone-600 font-satoshi font-medium mt-3 ml-3">
+                            Total: ₦ <span>{price}</span>
+                        </p>
+                    </div>
 
                     <label className="block w-full">
                         <span className='text-slate-600 text-sm'>Access to a computer</span>
                         <div className="flex items-center mt-2">
-                            <input className="form-checkbox block" type="checkbox" checked={formData.computer_access} onChange={(e) => setFormData((prev => ({...prev, computer_access: e.target.checked})))} />
+                            <input className="form-checkbox block" type="checkbox" checked={formData.computer_access} onChange={(e) => setFormData((prev => ({ ...prev, computer_access: e.target.checked })))} />
                             <span className="text-slate-500 text-xs ml-2">Do you have access to a working computer?</span>
                         </div>
                     </label>
@@ -212,7 +281,7 @@ function RegisterForm({ submitUrl, formTitle, formDesc, formFooter }: PropsType)
                 </form>
 
                 <p className='text-sm text-slate-500 py-10 block text-center'>
-                    { formFooter && formFooter }
+                    {formFooter && formFooter}
                 </p>
             </div>
         </div>
