97124fc1a6f4e1995f9f0fb2832bb6dbb817ab68
Godswill Nasara <godswillnlm@gmail.com>
2023-10-20 02:09:39 +0100

    Feature: add check for email before creating profile

--- app/api/pg-cohort/signup/route.ts
+++ app/api/pg-cohort/signup/route.ts
@@ -1,12 +1,16 @@
 import { connectToDB } from "@/utils/database";
 import Profile from "@/models/profile";
+import type { FormDataType } from "@/app/types";
 
 export const POST = async (
   req: Request,
 ) => {
   try {
     await connectToDB();
-    const data = await req.json();
+    const data: FormDataType = await req.json();
+
+    const emailExists = await Profile.findOne({ email: data.email });
+    if (emailExists) return new Response("Email already in use.", { status: 409 });
 
     const newProfile = new Profile({ ...data });
     newProfile.save();
