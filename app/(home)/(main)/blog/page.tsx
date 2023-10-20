d293115fbc27261baf13d68087542e5b58e3ebd7
Godswill Nasara <godswillnlm@gmail.com>
2023-10-20 02:13:02 +0100

    Chore: remove redundant files

--- app/(home)/(main)/blog/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function BlogsPage() {
-    return (
-        <div>
-            <p className="font-semibold text-5xl text-center">Blogs page</p>
-        </div>
-    );
-}
--- app/(home)/(main)/blog/types/index.ts
+++
@@ -1,7 +0,0 @@
-export interface Blog {
-  title: string;
-  id: number;
-  description: string;
-  link: string;
-  imageUrl?: string;
-}
