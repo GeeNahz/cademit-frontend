106d4d025a0ebd75964f770e7e53853f47284d6c
Godswill Nasara <godswillnlm@gmail.com>
2023-10-20 02:04:34 +0100

    Update: add more types for application

--- app/types/index.ts
+++ app/types/index.ts
@@ -15,3 +15,36 @@ export interface IconObj {
   id: number;
   icon: React.ReactNode;
 }
+
+export const PAYMENT_APPROACH = {
+  FULL_PAYMENT: "Full payment",
+  PER_MODULE: "Pay per module",
+};
+
+export const FETCH_STATUS = {
+  IDLE: "idle",
+  LOADING: "loading",
+  SUCCESS: "success",
+  ERROR: "error",
+};
+
+export type PaymentApproach = "Full payment" | "Pay per module" | "";
+
+export type FormDataType = {
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone?: string;
+  employment_status: string;
+  course: string;
+  experience_level: number;
+  computer_access: boolean;
+  internet_access: boolean;
+  use_workspace: boolean;
+  payment_approach: PaymentApproach;
+  module: string;
+  paid: boolean;
+  reference?: string;
+}
+
+export type StatusType = "info" | "success" | "error" | "idle";
