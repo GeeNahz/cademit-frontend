c4d8da7a8f9c67df51a94ffc3858b02f70ff8324
Godswill Nasara <godswillnlm@gmail.com>
2023-10-20 01:58:08 +0100

    Refactor: update the navbar for dashboard

--- app/dashboard/components/Navbar.tsx
+++ app/dashboard/components/Navbar.tsx
@@ -1,6 +1,29 @@
+import Image from "next/image";
+
 const Navbar = () => {
+  const user = {
+    username: "",
+    email: "",
+    image: "",
+  };
+
   return (
-    <div className="h-[3.75rem] sticky top-0 z-50 bg-white">Navbar</div>
+    <nav className="px-5 h-[3.75rem] w-full sticky top-0 z-50 bg-sky-200 flex items-center justify-between text-lg">
+      <div className="logo w-[145px] h-[51.55px] flex items-center">
+        <Image
+          src="/logo.png"
+          alt="CADEMit"
+          height={100}
+          width={200}
+          className="mt-3 object-cover"
+        />
+      </div>
+
+      <div className="user-profile flex gap-5 items-center">
+        <p className="profile-email">myemail@example.com</p>
+        <div className="profile-image bg-base-content rounded-full h-11 w-11"></div>
+      </div>
+    </nav>
   )
 }
 
