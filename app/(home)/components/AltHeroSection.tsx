3e71e03fd29bb3fee9200d185cfb3ef6e32914d3
Godswill Nasara <godswillnlm@gmail.com>
2023-10-20 02:07:01 +0100

    Feature: create a redesign for hero section

--- app/(home)/components/AltHeroSection.tsx
+++ app/(home)/components/AltHeroSection.tsx
@@ -26,16 +26,16 @@ const AltHeroSection = () => {
     return (
         <main className="min-h-[720px] w-full max-w-[calc(1234px + 56px * 2)] text-center mt-0 sm:mt-20 mb-0 mx-auto py-0 px-[32px] sm:px-[56px] overflow-hidden isolate border-b border-stone-300 flex flex-col items-center justify-center">
             <div>
-                <h1 className="p-6 font-extrabold leading-[1] tracking-tighter hero-title-font-size gradient-text text-center sm:text-start ml-0 sm:ml-14">The Tech Plug <span className="block md:hidden"></span> 
+                <h1 className="p-6 font-extrabold leading-[1] tracking-tighter hero-title-font-size gradient-text">The Tech Plug <span className="block md:hidden"></span> 
                 <div className="hidden" ref={typedStrings}>
-                    <p>You Can Trust</p>
-                    <p>that is guranteed</p>
-                    <p>For Everyone</p>
+                    <p>You Can Trust.</p>
+                    <p>that is Guranteed.</p>
+                    <p>For Everyone.</p>
                 </div>
                 <span ref={typedEl}></span></h1>
 
                 <div className="py-6 px-8 text-stone-500 font-normal leading-[1.8] tracking-[-0.01em] left-px w-[calc(100% - 1px *2)] hero-subtitle-font-size">
-                    <p>We are you reliable source for all things technological. <strong className="text-stone-900 font-medium">Let our expertise be your guide</strong> to seamless innovation and digital empowerment.</p>
+                    <p>We are your reliable source for all things technological. <strong className="text-stone-900 font-medium">Let our expertise be your guide</strong> to seamless innovation and digital empowerment.</p>
                 </div>
 
                 <div className="hero-footer h-full flex flex-col items-center justify-start gap-4 p-0">
