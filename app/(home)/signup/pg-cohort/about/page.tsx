7174f6fa4384bc0628a734dca2fe75afb6d3ff0f
Godswill Nasara <godswillnlm@gmail.com>
2023-10-20 01:47:40 +0100

    Feature: create about page for pg cohort

---
+++ app/(home)/signup/pg-cohort/about/page.tsx
@@ -0,0 +1,35 @@
+export default function AboutPGCohort() {
+    return (
+        <div className="container mx-auto mt-16 text-justify">
+            <h2 className="header text-4xl font-satoshi font-black">About Data Science PG Cohort</h2>
+            <p className="my-5">
+                The Data Science PG Cohort is an intensive and comprehensive program designed to equip aspiring data scientists with the knowledge, skills, and practical experience needed to excel in the dynamic field of data science. This cohort-based program brings together a select group of motivated individuals who share a passion for data analysis, machine learning, and statistics.</p>
+
+            <p className="my-5">
+                Throughout the Data Science PG Cohort, participants embark on a structured and immersive learning journey that covers a wide spectrum of topics essential for a successful career in data science. The curriculum is meticulously crafted and regularly updated to align with industry trends and best practices. Participants delve into areas such as data preprocessing, statistical analysis, data visualization, machine learning, deep learning, natural language processing, and big data technologies.
+            </p>
+
+            <div className="my-8">
+                <p>Key features of the Data Science PG Cohort include:</p>
+                <ol className="list-decimal pl-5 my-5">
+
+                    <li> Expert Instructors: Participants learn from seasoned data science professionals and academics who bring real-world experience and deep expertise to the program.</li>
+
+                    <li>Hands-on Projects: The cohort emphasizes practical, project-based learning, enabling participants to apply their knowledge to real data sets and industry-relevant challenges.</li>
+
+                    <li>Peer Learning: Cohort members collaborate, share insights, and engage in peer-to-peer learning, fostering a supportive and collaborative community.</li>
+
+                    <li> Industry Partnerships: The program often collaborates with leading companies and organizations, providing participants with opportunities for internships, networking, and exposure to real-world data science applications.</li>
+
+                    <li> Career Support: The Data Science PG Cohort offers career guidance, resume workshops, and interview preparation to help participants launch or advance their careers in data science.</li>
+
+                    <li>Flexibility: While rigorous, the program is designed to accommodate different backgrounds and schedules, allowing participants to balance their studies with other commitments.</li>
+
+                    <li> Graduation and Certification: Upon successful completion of the cohort, participants receive a prestigious certification or postgraduate degree in data science, enhancing their credibility in the job market.</li>
+
+                </ol>
+                <p>The Data Science PG Cohort is ideal for individuals who are committed to mastering the art and science of data analysis, leveraging cutting-edge technologies to solve complex problems, and making a meaningful impact in various industries, from finance and healthcare to e-commerce and beyond.</p>
+            </div>
+        </div>
+    )
+}
