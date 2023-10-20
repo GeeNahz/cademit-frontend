a771b55523b75a4862612c48b704d8785a82fe00
Godswill Nasara <godswillnlm@gmail.com>
2023-10-20 02:41:52 +0100

    Refactor: swapped the provider for paystack

--- app/hooks/paystack.ts
+++ app/hooks/paystack.ts
@@ -1,7 +1,5 @@
-import { useState } from "react";
 import { usePaystackPayment } from "react-paystack";
-import { useRouter } from "next/navigation";
-import PaystackPop from '@paystack/inline-js';
+// import PaystackPop from '@paystack/inline-js';
 
 
 type TPaystackProps = {
@@ -17,9 +15,18 @@ type TPaystackProps = {
 };
 
 function usePaystack({ reference, email, amount, firstname, lastname, phone, onSuccessfulPay }: TPaystackProps) {
+  // const config = {
+  //   reference: reference ? reference : (new Date()).getTime().toString(),
+  //   key: process.env.NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY as string,
+  //   email,
+  //   amount,
+  //   firstname,
+  //   lastname,
+  //   phone,
+  // };
   const config = {
     reference: reference ? reference : (new Date()).getTime().toString(),
-    key: process.env.NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY as string,
+    publicKey: process.env.NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY as string,
     email,
     amount,
     firstname,
@@ -27,21 +34,31 @@ function usePaystack({ reference, email, amount, firstname, lastname, phone, onS
     phone,
   };
 
+  function onSuccess(ref?: any) {
+    if (onSuccessfulPay) onSuccessfulPay(true, ref);
+  }
+  function onClose() {
+    console.log("Payment terminated.");
+  }
+
+  const payment = usePaystackPayment(config);
+  
   function initializePayment() {
-    try {
-      const paystack = new PaystackPop();
-      paystack.newTransaction({
-        ...config,
-        onSuccess: (transaction: any) => {
-          if (onSuccessfulPay) onSuccessfulPay(true, transaction.reference);
-        },
-        onClose: () => {
-          console.log("Payment was terminated.");
-        },
-      });
-    } catch (error) {
-      console.log("Payment error: ", error);
-    }
+    payment(onSuccess, onClose);
+    // try {
+    //   const paystack = new PaystackPop();
+    //   paystack.newTransaction({
+    //     ...config,
+    //     onSuccess: (transaction: any) => {
+    //       if (onSuccessfulPay) onSuccessfulPay(true, transaction.reference);
+    //     },
+    //     onClose: () => {
+    //       console.log("Payment was terminated.");
+    //     },
+    //   });
+    // } catch (error) {
+    //   console.log("Payment error: ", error);
+    // }
   }
   return { initializePayment };
 }
