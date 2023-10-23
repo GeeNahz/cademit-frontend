6d6eee860d6b7a719e8df0fe5f56ddd46e9f9853
Godswill Nasara <godswillnlm@gmail.com>
2023-10-23 21:25:27 +0100

    Refactor: extract the generate form fxn as a util fxn

--- app/(home)/signup/components/Form.tsx
+++ app/(home)/signup/components/Form.tsx
@@ -3,8 +3,9 @@
 import { Dispatch, FormEvent, SetStateAction, useEffect, useRef, useState } from "react";
 import clsx from "clsx";
 
-import { FormField, FormSelectOptions, INPUT_TYPES } from "@/app/types";
+import { FormField } from "@/app/types";
 
+import { generateFormFields } from "@/utils/forms";
 
 type FormProps = {
     header?: string;
@@ -18,103 +19,6 @@ type FormProps = {
     setData: Dispatch<SetStateAction<any>>;
     handleSubmit: (e: FormEvent) => any;
 };
-
-function generateFormFields(field: FormField, data: {} | any, setData: Dispatch<SetStateAction<any>>, idx: string | number) {
-    switch (field.type) {
-        case INPUT_TYPES.TEXT:
-        case INPUT_TYPES.EMAIL:
-        case INPUT_TYPES.TEL:
-        case INPUT_TYPES.PASSWORD:
-            return (
-                <label key={field.fieldName} htmlFor={field.fieldProps?.id} className="w-full">
-                    <span className="font-satoshi font-semibold text-base text-gray-700">{field.label}</span>
-
-                    <input
-                        type={field.type}
-                        id={field.fieldProps?.id}
-                        value={(field.value as string | number)}
-                        onChange={(e) => setData({ ...data, [field.fieldName]: e.target.value })}
-                        placeholder={field.fieldProps?.placeholder}
-                        required={field.fieldProps?.required}
-                        className="form_input"
-                    />
-                </label>
-            );
-        case INPUT_TYPES.TEXTAREA:
-            return (
-                <label key={field.fieldName} htmlFor={field.fieldProps?.id} className="w-full">
-                    <span className="font-satoshi font-semibold text-base text-gray-700">{field.label}</span>
-
-                    <textarea
-                        name={field.fieldName}
-                        id={field.fieldProps?.id}
-                        cols={30}
-                        rows={10}
-                        value={field.value as string}
-                        onChange={(e) => setData({ ...data, [field.fieldName]: e.target.value })}
-                        required={field.fieldProps?.required}
-                        placeholder={field.fieldProps?.placeholder}
-                        className="form_textarea"
-                    ></textarea>
-                </label>
-            );
-        case INPUT_TYPES.RADIO:
-            return (
-                <label key={field.fieldName}>
-                    <span className="font-satoshi font-semibold text-base text-gray-700">{field.label}</span>
-
-                    {
-                        (field?.selectOptions as []).map((option: FormSelectOptions) => (
-                            <div key={option.id} className="mb-1">
-                                <input
-                                    type={field.type}
-                                    name={field.fieldName}
-                                    value={(option.value).toString()}
-                                    onChange={(e) => setData({ ...data, [field.fieldName]: parseInt(e.target.value) })}
-                                    required
-                                />
-                                <span className="ml-3">{option.value} &nbsp; {option.title}</span>
-                            </div>
-                        ))
-                    }
-                </label>
-            );
-        case INPUT_TYPES.CHECKBOX:
-            return (
-                <label key={field.fieldName} htmlFor={field.fieldProps?.id} className="w-full flex items-center gap-3">
-                    <input
-                        type={field.type}
-                        id={field.fieldProps?.id}
-                        checked={field.value as boolean}
-                        onChange={(e) => setData({ ...data, [field.fieldName]: e.target.checked })
-                        }
-                        className="rounded border border-stone-400"
-                        required={field.fieldProps?.required}
-                    />
-                    <span className="font-satoshi font-normal text-base text-gray-500">{field.label}</span>
-                </label>
-            );
-        case INPUT_TYPES.SELECT:
-            return (
-                <label key={field.fieldName} htmlFor={field.fieldProps?.id} className="w-full">
-                    <span className="font-satoshi font-semibold text-base text-gray-700">{field.label}</span>
-
-                    <select name={field.fieldName} id={field.fieldProps?.id} onChange={(e) => setData({ ...data, [field.fieldName]: e.target.value })} value={field.value as string} required={field.fieldProps?.required} className="form_input">
-                        <option value="">-- Select {field.label} --</option>
-                        {
-                            (field.selectOptions as []).map((option: FormSelectOptions) => (
-                                <option key={option.id} value={(option.value as string | number)}>{option.title}</option>
-                            ))
-                        }
-                    </select>
-                </label>
-            );
-        default:
-            return (
-                <p key={idx} className="text-red text-lg font-semibold text-center py-5">Please provide a field with valid input type: {field.type}</p>
-            );
-    };
-}
 
 export default function Form({ type, header, desc, fields, submitting, data, message, messageType, setData, handleSubmit, }: FormProps) {
     const formEl = useRef<HTMLFormElement>(null);
