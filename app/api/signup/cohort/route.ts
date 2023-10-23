44f72e35d4b4a056402aaac123d68bbaa10cd19b
Godswill Nasara <godswillnlm@gmail.com>
2023-10-23 21:11:19 +0100

    Feature: create route to create student cohort record

---
+++ app/api/signup/cohort/route.ts
@@ -0,0 +1,38 @@
+import { connectToDB } from "@/utils/database";
+import Student from "@/models/student";
+import { StudentRecord } from "@/app/types";
+
+async function create(model: any, payload: any) {
+  const newRecord = new model({ ...payload });
+  const createdRecord = await newRecord.save();
+  return createdRecord;
+}
+
+export const POST = async (req: Request) => {
+  const {
+    first_name,
+    last_name,
+    email,
+    phone,
+    course,
+    employment_status,
+    purpose,
+    experience_level,
+    computer_access,
+    internet_access,
+    use_workspace,
+  } = await req.json();
+
+  try {
+    await connectToDB();
+    
+    const payload: StudentRecord = { first_name, last_name, email, phone, course, employment_status, experience_level, purpose, computer_access, internet_access, use_workspace };
+
+    const student = await create(Student, payload);
+    console.log("Created student: ", student);
+
+    return new Response(JSON.stringify(payload), { status: 201 });
+  } catch (error) {
+    return new Response("An error occured: " + error, { status: 500 });
+  }
+};
