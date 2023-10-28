637ce2b6d95c643364e7e6a1839f9375d93a25ee
Godswill Nasara <godswillnlm@gmail.com>
2023-10-28 21:56:18 +0100

    Feature: add signin form to signin page

--- app/(home)/signin/page.tsx
+++ app/(home)/signin/page.tsx
@@ -1,7 +1,67 @@
+"use client";
+
+import { FormEvent, useState } from "react";
+import { signIn } from "next-auth/react";
+
+import Form from "@/app/components/Form";
+
+import { FormField, FETCH_STATUS } from "@/app/types";
+
 export default function Signin() {
+    const [data, setData] = useState({
+        username: "",
+        password: "",
+    });
+    const [status, setStatus] = useState(FETCH_STATUS.IDLE);
+
+    const signInFormFields: FormField[] = [
+        {
+            type: "text",
+            fieldName: "username",
+            label: "Username",
+            value: data.username,
+            fieldProps: {
+                classes: "username",
+                id: "username",
+                placeholder: "johndoe",
+                required: true,
+            },
+        },
+        {
+            type: "password",
+            fieldName: "password",
+            label: "Password",
+            value: data.password,
+            fieldProps: {
+                classes: "pword",
+                id: "password",
+                placeholder: "●●●●●●●●●●●●●●●",
+                required: true,
+            },
+        },
+    ];
+
+    function handleSignin(e: FormEvent) {
+        e.preventDefault();
+
+        // signIn("credentials", {
+        //     password: data.password,
+        //     username: data.username,
+        // });
+    }
+
     return (
         <div className="signin-class">
-            <p className="text-lg font-bold font-lora text-stone-600">Sign in page</p>
+            <Form
+                fields={signInFormFields}
+                data={data}
+                setData={setData}
+                handleSubmit={handleSignin}
+                submitting={status === FETCH_STATUS.LOADING}
+                type="Sign in"
+                header="Sign in"
+                desc="Sign in to your account"
+            />
         </div>
     );
 }
