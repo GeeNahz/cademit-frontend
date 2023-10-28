4675f8cfebe039de6e616188fd5993ae720488b2
Godswill Nasara <godswillnlm@gmail.com>
2023-10-28 16:05:03 +0100

    Feature: create signin page

---
+++ app/(home)/signin/page.tsx
@@ -0,0 +1,7 @@
+export default function Signin() {
+    return (
+        <div className="signin-class">
+            <p className="text-lg font-bold font-lora text-stone-600">Sign in page</p>
+        </div>
+    );
+}
