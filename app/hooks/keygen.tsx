5028e95b228860ca0c276aeed02b74b28f6d4d27
Godswill Nasara <godswillnlm@gmail.com>
2023-10-28 22:02:28 +0100

    Feature: create fxns to hash and compare passwords

--- app/hooks/keygen.tsx
+++ app/hooks/keygen.tsx
@@ -1,6 +1,16 @@
 import { FormDataType } from "@/app/types";
+import bcrypt from "bcrypt";
 
 
 export function useGenerateReferenceKey(data: FormDataType) {    
     return (new Date()).getDate().toLocaleString() + (new Date()).getMilliseconds().toLocaleString() + "-" + (data.first_name.substring(0,3)).toLocaleLowerCase() + (data.last_name.substring(0,3)).toLocaleLowerCase() + "-" + (new Date()).getMilliseconds().toLocaleString()
+}
+
+export function useHashPassword(password: string) {
+    const saltRounds = 10;
+    return bcrypt.hashSync(password, saltRounds);
+}
+
+export function useCheckHashPassword(password: string, hashed_password: string) {
+    return bcrypt.compareSync(password, hashed_password);
 }
