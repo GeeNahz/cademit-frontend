1ef1c385fc2a257641b307006d52b0bea8e25ab0
Godswill Nasara <godswillnlm@gmail.com>
2023-10-30 00:52:42 +0100

    Feature: create route to get single prospect details

---
+++ app/api/v1/prospects/[id]/route.ts
@@ -0,0 +1,27 @@
+import { Types } from "mongoose";
+
+import Prospect from "@/models/prospects";
+import { connectToDB } from "@/utils/database";
+
+import { status } from "@/utils/status";
+
+
+export const GET = async (req: Request, { params }: { params: Types.ObjectId}) => {
+  try {
+    await connectToDB();
+    
+    const prospect = await Prospect.findOne({
+      _id: params.id,
+    });
+
+    console.log("SERVER LOG: ", prospect);
+
+    if (prospect) {
+      return new Response(JSON.stringify(prospect), { status: status.HTTP_200_OK });
+    } else {
+      return new Response("No record found with the provided id", { status: status.HTTP_404_NOT_FOUND });
+    }
+  } catch (error) {
+    return new Response("Server error " + error, { status: status.HTTP_500_INTERNAL_SERVER_ERROR });
+  }
+};
