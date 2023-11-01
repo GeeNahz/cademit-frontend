6bd76698175c78aa15121f4cf2a41cfcd07b9592
Godswill Nasara <godswillnlm@gmail.com>
2023-11-01 12:38:57 +0100

    Fix: backdrop covering all content

--- app/components/Backdrop.tsx
+++ app/components/Backdrop.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const Backdrop = ({ onClick, isActive }: Props) => {
     const backdropFade = clsx(
-        "absolute z-0 top-0 left-0 right-0 bottom-0 h-full min-h-screen w-screen bg-black transition-all",
+        "absolute -z-10 top-0 left-0 right-0 bottom-0 h-full min-h-screen w-screen bg-black transition-all",
         {
             "bg-opacity-40 duration-100 block backdrop-blur-sm": isActive,
             "bg-opacity-0 duration-200 delay-300 hidden": !isActive,
