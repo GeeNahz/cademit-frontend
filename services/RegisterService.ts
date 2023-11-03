91125c089db57be326f8f2c9bb9a4388c3eecc8d
Godswill Nasara <godswillnlm@gmail.com>
2023-11-03 20:07:59 +0100

    Refactor: complying with DRY principle

--- services/RegisterService.ts
+++ services/RegisterService.ts
@@ -1,28 +1,14 @@
-import { ErrorType, User } from "@/app/types";
+import BaseService from "./BaseService";
+import { User } from "@/app/types";
 
 export async function registerUser(data: User) {
-  try {
-    const response = await fetch("/api/auth/register/user", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
 
-    if (response.ok) {
-      return await response.json();
-    } else {
-      const error: ErrorType = {
-        status: response.status,
-        statusText: response.statusText,
-        headers: response.headers,
-        errorMessage: await response.text(),
-      };
-
-      throw error;
-    }
-  } catch (error) {
-    throw error;
-  }
+  return await BaseService("/api/auth/register/user", options);
 }
