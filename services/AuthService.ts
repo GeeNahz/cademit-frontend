9e610baff831923e71db3ad46b3951be616d4159
Godswill Nasara <godswillnlm@gmail.com>
2023-11-04 01:13:37 +0100

    Feature: create password reset service

--- services/AuthService.ts
+++ services/AuthService.ts
@@ -1,5 +1,6 @@
 import BaseService from "./BaseService";
 import { User } from "@/app/types";
+import { status } from "@/utils/status";
 
 export async function registerUser(data: User) {
   const options = {
@@ -11,4 +12,32 @@ export async function registerUser(data: User) {
   };
 
   return await BaseService("/api/auth/register/user", options);
+}
+
+type PasswordResetType = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+export async function resetPassword(data: PasswordResetType) {
+  if (data.password !== data.confirmPassword) {
+    console.log("I RAN");
+    
+    throw {
+      status: status.HTTP_400_BAD_REQUEST,
+      statusText: "Bad Request",
+      headers: {},
+      errorMessage: "Passwords do not match",
+    };
+  }
+
+  const options: RequestInit = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email: data.email, password: data.password }),
+  };
+
+  return await BaseService("/api/auth/password-reset", options);
 }
